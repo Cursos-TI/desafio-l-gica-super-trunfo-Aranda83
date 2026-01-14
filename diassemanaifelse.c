f7f7f3bb9ea159ef506f1d9eedff83e8088ffe15
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-14 07:59:37 +0000

    dias if else

---
+++ diassemanaifelse.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+   int dia;
+
+   printf("entre o valor do dia!\n");
+   scanf("%d", &dia);
+
+   if (dia == 1)
+    printf("Domingo\n");
+    else if (dia == 2)
+    printf("Segunda\n");
+    else if (dia== 3)
+    printf("terça\n");
+    else if (dia == 4)
+    printf("quarta- feira\n");
+    else if(dia == 5)
+    printf("quinta\n");
+    else if (dia == 6)
+    printf("sexta\n");
+    else if (dia == 7)
+    printf("sábado\n");
+
+
+
+
+
+
+
+
+}
