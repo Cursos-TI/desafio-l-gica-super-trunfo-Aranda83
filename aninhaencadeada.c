442a0d2a001cd0a0eaecae6aea722500e232aed2
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-14 06:38:58 +0000

    criei par/impar

---
+++ aninhaencadeada.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int numero;
+
+    printf("digite o número: ");
+    scanf("%d", &numero);
+
+    if ( numero > 0)
+    if (numero % 2 == 0)
+    printf("numero par!\n");
+    else
+    printf("numero impar\n");
+    
+    if (numero > 0)
+    
+    printf("positivo!\n");
+    else if (numero == 0)
+    printf("numero é zero!\n");
+    else
+    printf("negativo!\n");
+
+
+
+
+
+
+
+
+}
