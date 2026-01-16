0e174dc85dd2bd2e102542786edf9d6ac4100141
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-16 06:00:01 +0000

    opternario simples

---
+++ operadorternario.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 18;
+    int resultado;
+
+    idade >= 18 ? printf("voce é maior de idade!\n") : printf("voce é menor de idade!\n");
+
+
+
+
+
+
+
+
+
+}
