ad5a048cd5b5b51e26c227795c4de812fa5ba5ed
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-16 06:40:28 +0000

    comparando num

---
+++ comparandonumifelse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int num1 = 30, num2 = 20;
+int maior;
+
+num1 > num2 ? (maior = num1) : (maior = num2);
+
+printf("o numero maior é: %d\n", maior);
+
+
+return 0;
+
+
+}
