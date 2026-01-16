3866e63668ca079337a3b6ae00875f02599733f4
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-16 06:20:32 +0000

    temp if else

---
+++ temperaturaifelse.c
@@ -0,0 +1,23 @@
+#include <stdio_ext.h>
+
+int main(){
+
+int temperatura = 19;
+int resultado;
+
+resultado = temperatura >= 20 ? 1 : 0;
+
+
+if(resultado == 1){
+printf("hj está calor!\n");
+
+} else {
+    printf("hj está frio!\n");
+}
+
+
+}
+
+
+
+
