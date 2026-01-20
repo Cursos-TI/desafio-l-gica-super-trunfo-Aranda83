eafa6203b39419c795a256ffa271016dd2d56600
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-20 05:01:18 +0000

    for 10 em 10

---
+++ for10em10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    for (int i = 0; i <= 100; i += 10 ) // Variável; condição; incremento
+
+    {                            // chaves p começar o bloco
+
+      printf("%d\n", i);         // executar
+
+    }                            // chaves p fechar o bloco
+    return 0;
+}
