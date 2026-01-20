34a4fcd557528345114b5e20177404b55bc53862
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-20 04:40:07 +0000

    for multiplos

---
+++ formultiplos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    for (int i = 100; i >= 1; i-=10 ) // Variável; condição; incremento
+
+    {                            // chaves p começar o bloco
+        
+       printf("%d\n", i);
+
+    }                            // chaves p fechar o bloco
+
+    return 0;
+}
