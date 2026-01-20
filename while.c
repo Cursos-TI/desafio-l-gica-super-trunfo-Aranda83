25afd84acba4a115eaec2c34df65d70c2c908835
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-20 04:30:41 +0000

    while

--- while.c
+++ while.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
+#include <stdio.h>     // Biblioteca
 
 int main(){
 
-int i = 1;
-
-// nome da estrutura
+int i = 1;            // Variável
 
 
-while (i <= 20)
-{
+while (i <= 20)       // estrutura
+{                     // abre chaves p o código
 
-printf("%d\n", i);
-i++;
+printf("%d\n", i);    // executar
+i++;                  // incremento
 
 
-}
+}                    // fecha chaves do código
 
 
 return 0;
