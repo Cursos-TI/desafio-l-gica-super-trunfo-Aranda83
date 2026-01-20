bf56a67da879baaa469ba9e70e2c05b77bee1d96
Aranda83 <92658125+Aranda83@users.noreply.github.com>
2026-01-20 04:53:04 +0000

    for par

---
+++ forpares.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    for (int i = 1; i <= 100; i++)   // Variável; condição; incremento
+                                    
+        if (i % 2 == 0)              // checar se é par
+{                                    // chaves p começar o bloco
+    printf("%d\n", i);               // executar
+}                                    // fecha bloco
+    
+
+    }
